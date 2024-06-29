bc082cd4b4f6ea9a89617d300ab9b19907495d85
oneye5 <ocjlaz@gmail.com>
2024-06-29 12:34:10 +1200

    moller trumbore algorithm implementation for raycasts

--- Raytracer_allegro/my_3d_lib.h
+++ Raytracer_allegro/my_3d_lib.h
@@ -2,7 +2,10 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
-
+/*
+	this file contains all smaller helper data structures and functions relating to
+	the renderer that dont deserve to have their own file.
+*/
 
 template<class T>
 class Color
@@ -14,7 +17,6 @@ public:
 		r = red; g = green; b = blue;
 	}
 };
-
 
 template<class T>
 class vec2
@@ -58,8 +60,43 @@ public:
 		y = XYZ_ASSIGNMENT;
 		z = XYZ_ASSIGNMENT;
 	}
+
+	static inline vec3<T> subtract(const vec3<T>& v1, const vec3<T>& v2)
+	{
+		return vec3<T>{v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
+	}
+	static inline vec3<T> add(const vec3<T>& v1, const vec3<T>& v2)
+	{
+		return vec3<T>{v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+	}
+	static inline vec3<T> cross_product(const vec3<T>& v1, const vec3<T>& v2)
+	{
+		return vec3<T>
+		{
+			v1.y* v2.z - v1.z * v2.y,
+			v1.z* v2.x - v1.x * v2.z,
+			v1.x* v2.y - v1.y * v2.x
+		};
+	}
+	static inline T dot_product(const vec3<T>& v1, const vec3<T>& v2)
+	{
+		return
+			v1.x * v2.x +
+			v1.y * v2.y +
+			v1.x * v2.z;
+	}
+	static inline T multiply(const vec3<T>& v1, const T& f)
+	{
+		return vec3<T>
+		{
+			v1.x* f,
+			v1.y* f,
+			v1.z* f
+		};
+	}
 };
 
+// gets a direction vector from a pitch and yaw value
 template<class T>
 inline vec3<T> get_vector_from_angle(vec2<T> pitch_yaw)
 {
@@ -71,8 +108,9 @@ inline vec3<T> get_vector_from_angle(vec2<T> pitch_yaw)
 
 	return vec3<T>(x,y,z);
 }
+// returns the distance between two points
 template<class T>
-inline T distnce_between(vec3<T> p1, vec3<T> p2)
+inline T distance_between(vec3<T> p1, vec3<T> p2)
 {
 	return std::sqrtf
 	(
@@ -164,9 +202,9 @@ public:
 
 			float averge_depth =
 				(
-					distnce_between(v1, camera_position) +
-					distnce_between(v2, camera_position) +
-					distnce_between(v3, camera_position) 
+					distance_between(v1, camera_position) +
+					distance_between(v2, camera_position) +
+					distance_between(v3, camera_position) 
 				) / 3.0f;
 
 			tris.push_back({ averge_depth, {
@@ -178,8 +216,7 @@ public:
 
 
 
-		std::sort(tris.begin(), tris.end(),
-			[](const auto& a, const auto& b) { return a.first > b.first; });
+		std::sort(tris.begin(), tris.end(),[](const auto& a, const auto& b) { return a.first > b.first; });
 
 		// Update the index buffer
 		index_buffer.clear();
@@ -194,3 +231,104 @@ class Scene
 public:
 	std::vector<Mesh> geometry;
 };
+
+/*
+* used for wrapping triangle information, to allow for flexibility in geometry implementation / format
+*/
+template <class VEC_TYPE, class UV_TYPE, class NORM_TYPE> 
+struct triangle_info
+{
+	vec3<VEC_TYPE>* vertices[3];
+	vec2<UV_TYPE>*	uv[3];
+	vec3<NORM_TYPE>* normals[3];
+};
+
+// used in ray_info, for avoiding un-needed memory allocation however we must be careful to avoid memory leaks
+template <class VEC_TYPE>
+struct hit_info
+{
+	VEC_TYPE DISTANCE;
+	vec3<VEC_TYPE> HIT_POINT_WORLDSPACE;
+	vec2<VEC_TYPE> HIT_POINT_UVSPACE;
+};
+// returned by ray_intersect_tri()
+template <class VEC_TYPE>
+class ray_info
+{
+public:
+	bool hit;
+	hit_info* hit_result;
+
+	ray_info(bool t, hit_info* hitInfo)
+	{
+		hit = t; 
+		hit_result = hitInfo;
+	}
+	ray_info(bool f)
+	{
+		if(f == true)
+			std::cout << "\nin ray_info(bool f), this constructor should only be called when the result is false, because hit information about the hit must be provided otherwise\n";
+		hit = f;
+	}
+	~ray_info()
+	{
+		if (hit_result != nullptr)
+			delete hit_result;
+	}
+};
+
+//checks if a ray intersects with a triangle. (uses the moller trumbore algorithm)
+template <class VEC_TYPE, class UV_TYPE, class NORM_TYPE>
+ray_info<VEC_TYPE> ray_intersect_tri(const vec3<float>& ray_origin, const vec3<float>& ray_dir, const triangle_info<VEC_TYPE, UV_TYPE, NORM_TYPE>& tri)
+{
+	const float moe = 0.000001;
+
+	vec3<VEC_TYPE> edge_1 = vec3<VEC_TYPE>::subtract(tri.vertices[1], tri.vertices[0]);
+	vec3<VEC_TYPE> edge_2 = vec3<VEC_TYPE>::subtract(tri.vertices[2], tri.vertices[0]);
+
+	vec3<VEC_TYPE> dir_edge_cross = vec3<VEC_TYPE>::cross_product(ray_dir, edge_2);
+	VEC_TYPE cross_edge_dot = vec3<VEC_TYPE>::dot_product(edge_1, dir_edge_cross);
+
+	if (cross_edge_dot > -moe && cross_edge_dot < moe) //ray parallel to triangle plane, it will never intersect so we can return early
+		return ray_info{ false };
+
+	VEC_TYPE inverse_cross_dot = 1.0 / cross_edge_dot;
+	vec3<VEC_TYPE> origin_tri_dif = vec3<VEC_TYPE>::subtract(ray_origin, tri.vertices[0]);
+	VEC_TYPE pos_dif_cross = vec3<VEC_TYPE>::cross_product(origin_tri_dif, edge_1);
+
+
+	VEC_TYPE intersect_u = inverse_cross_dot * vec3<VEC_TYPE>::dot_product(origin_tri_dif, dir_edge_cross);
+	VEC_TYPE intersect_v = inverse_cross_dot * vec3<VEC_TYPE>::dot_product(ray_dir, pos_dif_cross);
+	VEC_TYPE dist_traveled = inverse_cross_dot * vec3<VEC_TYPE>::dot_product(edge_2, pos_dif_cross);
+
+	if (intersect_u < 0.0 || intersect_u > 1.0)//--------------- point lies outside the bounds along the edge of the first fertex to the seccond, so we can return early
+		return ray_info{ false };
+
+	if (intersect_v < 0.0 || intersect_u + intersect_v > 1.0)//- point lies outside or past the third edge
+		return ray_info{ false };
+
+	if (dist_traveled < moe)//---------------------------------- ray does not travel far enough from origin to count as a valid hit
+		return ray_info{ false };
+
+	// we have a confirmed hit, now lets calculate where exactly this happened in world space
+	vec3<VEC_TYPE> world_pos = vec3<VEC_TYPE>::add
+	(
+		vec3<VEC_TYPE>::multiply(ray_dir, dist_traveled),
+		ray_origin
+	);
+
+	return ray_info
+	{ 
+		true,
+		new hit_info
+		{
+			dist_traveled,
+			world_pos,
+			vec2<VEC_TYPE>
+			{
+				intersect_u,
+				intersect_v
+			}
+		} 
+	}
+}
